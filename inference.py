aabffdbec2d79383016cf91c7fd4456c6a0aab8b
Anonymous <gramezan@gmail.com>
2025-05-16 22:23:06 -0700

    V27

--- inference.py
+++ inference.py
@@ -206,7 +206,7 @@ with st.sidebar:
     st.markdown(
         """
         <div style="text-align: center;">
-            <img src="https://fidesinnova.io/wp-content/uploads/2024/07/Logo-Captcha-Reduce-size.png" width="110">
+            <img src="https://fidesinnova.io/Logo-Captcha-Reduce-size.png" width="110">
             <div style="font-size: 28px; font-weight: bold; margin-top: 14px; color: white;">
                 Fides Innova<br>AI Agent
             </div>
